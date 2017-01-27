1804de8463b631280ed781bbcc0a758bc5b60276
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-27 19:08:00 +0100

    replace q with bluebird

--- test/jobProcessorTest.js
+++ test/jobProcessorTest.js
@@ -1,34 +1,32 @@
 require('should')
 var path = require('path')
-var q = require('q')
+var Promise = require('bluebird')
 var JobProcessor = require('../lib/jobProcessor')
 var Reporter = require('jsreport-core').Reporter
 
 describe('for jobProcessor', function () {
   var reporter
 
-  beforeEach(function (done) {
+  beforeEach(function () {
     reporter = new Reporter({
       rootDirectory: path.join(__dirname, '../')
     })
 
-    reporter.init().then(function () {
-      done()
-    }).fail(done)
+    return reporter.init()
   })
 
-  it('process should call handler and create task', function (done) {
+  it('process should call handler and create task', function () {
     this.timeout(3000)
     reporter.scheduling.stop()
 
-    reporter.documentStore.collection('schedules').insert({
+    return reporter.documentStore.collection('schedules').insert({
       cron: '*/1 * * * * *'
     }).then(function () {
       var counter = 0
 
       function exec () {
         counter++
-        return q()
+        return Promise.resolve()
       }
 
       var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
@@ -39,25 +37,24 @@ describe('for jobProcessor', function () {
         return reporter.documentStore.collection('tasks').find({}).then(function (tasks) {
           tasks.length.should.be.exactly(1)
           tasks[0].state.should.be.exactly('success')
-          tasks[0].finishDate.should.be.ok
-          done()
+          tasks[0].finishDate.should.be.ok()
         })
       })
-    }).catch(done)
+    })
   })
 
-  it('should not cross maxParallelJobs', function (done) {
+  it('should not cross maxParallelJobs', function () {
     this.timeout(2000)
     reporter.scheduling.stop()
 
-    reporter.documentStore.collection('schedules').insert({
+    return reporter.documentStore.collection('schedules').insert({
       cron: '*/1 * * * * *'
     }).then(function () {
       var counter = 0
 
       function exec () {
         counter++
-        return q()
+        return Promise.resolve()
       }
 
       var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
@@ -66,15 +63,14 @@ describe('for jobProcessor', function () {
       })
       return jobProcessor.process({waitForJobToFinish: true}).then(function () {
         counter.should.be.exactly(0)
-        done()
       })
-    }).catch(done)
+    })
   })
 
-  it('should recover failed tasks', function (done) {
+  it('should recover failed tasks', function () {
     reporter.scheduling.stop()
 
-    reporter.documentStore.collection('schedules').insert({
+    return reporter.documentStore.collection('schedules').insert({
       cron: '* * * * * 2090'
     }).then(function (schedule) {
       reporter.documentStore.collection('tasks').insert({
@@ -87,7 +83,7 @@ describe('for jobProcessor', function () {
 
       function exec () {
         counter++
-        return q()
+        return Promise.resolve()
       }
 
       var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
@@ -98,15 +94,14 @@ describe('for jobProcessor', function () {
 
       return jobProcessor.process({waitForJobToFinish: true}).then(function () {
         counter.should.be.exactly(1)
-        done()
       })
-    }).catch(done)
+    })
   })
 
-  it('should ping running tasks', function (done) {
+  it('should ping running tasks', function () {
     reporter.scheduling.stop()
 
-    reporter.documentStore.collection('schedules').insert({
+    return reporter.documentStore.collection('schedules').insert({
       cron: '* * * * * 2090'
     }).then(function (schedule) {
       return reporter.documentStore.collection('tasks').insert({
@@ -115,7 +110,7 @@ describe('for jobProcessor', function () {
         scheduleShortid: schedule.shortid
       }).then(function (task) {
         function exec () {
-          return q()
+          return Promise.resolve()
         }
 
         var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
@@ -126,11 +121,10 @@ describe('for jobProcessor', function () {
         return jobProcessor.process({waitForJobToFinish: true}).then(function () {
           return reporter.documentStore.collection('tasks').find({}).then(function (tasks) {
             tasks[0].ping.should.not.be.exactly(task.ping)
-            done()
           })
         })
       })
-    }).catch(done)
+    })
   })
 })
 
