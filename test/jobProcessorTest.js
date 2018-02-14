ee16435ced6eea54b5aaa68e0188153e45abc141
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2018-02-14 11:56:09 +0100

    rewrite to node 8 and jsreport 2

--- test/jobProcessorTest.js
+++ test/jobProcessorTest.js
@@ -1,187 +1,176 @@
 require('should')
-var path = require('path')
-var Promise = require('bluebird')
-var JobProcessor = require('../lib/jobProcessor')
-var Reporter = require('jsreport-core').Reporter
+const Promise = require('bluebird')
+const JobProcessor = require('../lib/jobProcessor')
+const jsreport = require('jsreport-core')
 
-describe('for jobProcessor', function () {
-  var reporter
-  var template
+describe('for jobProcessor', () => {
+  let reporter
+  let template
 
-  beforeEach(function () {
-    reporter = new Reporter({
-      rootDirectory: path.join(__dirname, '../'),
+  beforeEach(async () => {
+    reporter = jsreport({
       scheduling: {
         minScheduleInterval: 0
       }
     })
 
-    return reporter.init().then(function () {
-      return reporter.documentStore.collection('templates').insert({
-        content: 'foo',
-        engine: 'none',
-        recipe: 'html'
-      }).then(function (t) {
-        template = t
-      })
+    reporter.use(require('../')())
+    reporter.use(require('jsreport-templates')())
+    reporter.use(require('jsreport-reports')())
+
+    await reporter.init()
+    template = await reporter.documentStore.collection('templates').insert({
+      content: 'foo',
+      engine: 'none',
+      recipe: 'html'
     })
   })
 
-  it('process should call handler and create task', function () {
+  it('process should call handler and create task', async function () {
     this.timeout(3000)
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('schedules').insert({
+    await reporter.documentStore.collection('schedules').insert({
       cron: '*/1 * * * * *',
       templateShortid: template.shortid
-    }).then(function () {
-      var counter = 0
-
-      function exec () {
-        counter++
-        return Promise.resolve()
-      }
-
-      var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-        interval: 50,
-        maxParallelJobs: 1
-      })
-      return jobProcessor.process({waitForJobToFinish: true}).then(function () {
-        return reporter.documentStore.collection('tasks').find({}).then(function (tasks) {
-          tasks.length.should.be.exactly(1)
-          tasks[0].state.should.be.exactly('success')
-          tasks[0].finishDate.should.be.ok()
-          counter.should.be.exactly(1)
-        })
-      })
     })
+    let counter = 0
+
+    function exec () {
+      counter++
+      return Promise.resolve()
+    }
+
+    const jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 50,
+      maxParallelJobs: 1
+    })
+    await jobProcessor.process({waitForJobToFinish: true})
+    const tasks = await reporter.documentStore.collection('tasks').find({})
+    tasks.length.should.be.exactly(1)
+    tasks[0].state.should.be.exactly('success')
+    tasks[0].finishDate.should.be.ok()
+    counter.should.be.exactly(1)
   })
 
-  it('should not cross maxParallelJobs', function () {
+  it('should not cross maxParallelJobs', async function () {
     this.timeout(2000)
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('schedules').insert({
+    await reporter.documentStore.collection('schedules').insert({
       cron: '*/1 * * * * *',
       templateShortid: template.shortid
-    }).then(function () {
-      var counter = 0
-
-      function exec () {
-        counter++
-        return Promise.resolve()
-      }
-
-      var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-        interval: 50,
-        maxParallelJobs: 0
-      })
-      return jobProcessor.process({waitForJobToFinish: true}).then(function () {
-        counter.should.be.exactly(0)
-      })
     })
+    let counter = 0
+
+    function exec () {
+      counter++
+      return Promise.resolve()
+    }
+
+    const jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 50,
+      maxParallelJobs: 0
+    })
+    await jobProcessor.process({waitForJobToFinish: true})
+    counter.should.be.exactly(0)
   })
 
-  it('should recover failed tasks', function () {
+  it('should recover failed tasks', async () => {
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('schedules').insert({
+    const schedule = await reporter.documentStore.collection('schedules').insert({
       cron: '* * * * * 2090',
       templateShortid: template.shortid
-    }).then(function (schedule) {
-      reporter.documentStore.collection('tasks').insert({
-        ping: new Date(1),
-        state: 'running',
-        scheduleShortid: schedule.shortid
-      })
-    }).then(function () {
-      var counter = 0
-
-      function exec () {
-        counter++
-        return Promise.resolve()
-      }
-
-      var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-        interval: 20,
-        maxParallelJobs: 1,
-        taskPingTimeout: 10
-      })
-
-      return jobProcessor.process({waitForJobToFinish: true}).then(function () {
-        counter.should.be.exactly(1)
-      })
     })
+    await reporter.documentStore.collection('tasks').insert({
+      ping: new Date(1),
+      state: 'running',
+      scheduleShortid: schedule.shortid
+    })
+
+    let counter = 0
+
+    function exec () {
+      counter++
+      return Promise.resolve()
+    }
+
+    const jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 20,
+      maxParallelJobs: 1,
+      taskPingTimeout: 10
+    })
+
+    await jobProcessor.process({waitForJobToFinish: true})
+    counter.should.be.exactly(1)
   })
 
-  it('should ping running tasks', function () {
+  it('should ping running tasks', async () => {
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('schedules').insert({
+    const schedule = await reporter.documentStore.collection('schedules').insert({
       cron: '* * * * * 2090',
       templateShortid: template.shortid
-    }).then(function (schedule) {
-      return reporter.documentStore.collection('tasks').insert({
-        ping: new Date(new Date().getTime() - 1000),
-        state: 'running',
-        scheduleShortid: schedule.shortid
-      }).then(function (task) {
-        function exec () {
-          return Promise.resolve()
-        }
-
-        var jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-          interval: 20,
-          maxParallelJobs: 1
-        })
-        jobProcessor.currentlyRunningTasks.push(task)
-        return jobProcessor.process({waitForJobToFinish: true}).then(function () {
-          return reporter.documentStore.collection('tasks').find({}).then(function (tasks) {
-            tasks[0].ping.should.not.be.exactly(task.ping)
-          })
-        })
-      })
     })
+
+    const task = await reporter.documentStore.collection('tasks').insert({
+      ping: new Date(new Date().getTime() - 1000),
+      state: 'running',
+      scheduleShortid: schedule.shortid
+    })
+
+    function exec () {
+      return Promise.resolve()
+    }
+
+    const jobProcessor = new JobProcessor(exec, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 20,
+      maxParallelJobs: 1
+    })
+    jobProcessor.currentlyRunningTasks.push(task)
+
+    await jobProcessor.process({waitForJobToFinish: true})
+    const tasks = await reporter.documentStore.collection('tasks').find({})
+    tasks[0].ping.should.not.be.exactly(task.ping)
   })
 
-  it('_findTasksToRecover should skip tasks without schedules', function () {
+  it('_findTasksToRecover should skip tasks without schedules', async () => {
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('tasks').insert({
+    await reporter.documentStore.collection('tasks').insert({
       ping: new Date(1),
       state: 'running',
       scheduleShortid: 'invalid'
-    }).then(function () {
-      var jobProcessor = new JobProcessor(function () {}, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-        interval: 50,
-        maxParallelJobs: 1
-      })
-
-      return jobProcessor._findTasksToRecover()
-    }).then(function (tasks) {
-      tasks.should.have.length(0)
     })
+    const jobProcessor = new JobProcessor(function () {}, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 50,
+      maxParallelJobs: 1
+    })
+
+    const tasks = await jobProcessor._findTasksToRecover()
+
+    tasks.should.have.length(0)
   })
 
-  it('process should error schedules with missing templates', function () {
+  it('process should error schedules with missing templates', async function () {
     this.timeout(3000)
     reporter.scheduling.stop()
 
-    return reporter.documentStore.collection('schedules').insert({
+    await reporter.documentStore.collection('schedules').insert({
       cron: '*/1 * * * * *',
       templateShortid: 'invalid'
-    }).then(function () {
-      var jobProcessor = new JobProcessor(function () { }, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
-        interval: 50,
-        maxParallelJobs: 1
-      })
-      return jobProcessor.process({waitForJobToFinish: true}).then(function () {
-        return reporter.documentStore.collection('tasks').find({}).then(function (tasks) {
-          tasks.length.should.be.exactly(1)
-          tasks[0].state.should.be.exactly('error')
-          tasks[0].error.should.containEql('Template')
-        })
-      })
     })
+
+    const jobProcessor = new JobProcessor(function () { }, reporter.documentStore, reporter.logger, reporter.scheduling.TaskType, {
+      interval: 50,
+      maxParallelJobs: 1
+    })
+
+    await jobProcessor.process({waitForJobToFinish: true})
+    const tasks = await reporter.documentStore.collection('tasks').find({})
+    tasks.length.should.be.exactly(1)
+    tasks[0].state.should.be.exactly('error')
+    tasks[0].error.should.containEql('Template')
   })
 })
